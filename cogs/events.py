e631647ee8c44c7188c3aaa0fe87e74b1e95d66f
Carson Brown <carsonbrown47@gmail.com>
2020-09-02 13:49:43 -0400

    moved events to cogs folder for organization

---
+++ cogs/events.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+class Events(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot;
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("py bot is online!")
+
+def setup(bot):
+    bot.add_cog(Events(bot))
