567150759250e20bec9f045b9a54e73969de0c42
Carson Brown <45776517+Zaymus@users.noreply.github.com>
2020-09-26 12:54:56 -0400

    added join, clear and list commands to interact with the team_list file

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import os
 
 class Commands(commands.Cog):
     def __init__(self, bot):
@@ -16,6 +17,48 @@ class Commands(commands.Cog):
             await ctx.send(f':satellite:```fix\nPONG!\n+ {ping}ms```:satellite_orbital:')
         else:
             await ctx.send(f':satellite:```diff\nPONG!\n- {ping}ms```:satellite_orbital:')
+
+    @commands.command()
+    async def join(self, ctx, team):
+        is_registered = False
+        with open("team_list.txt", "r") as file:
+                for line in file.readlines():
+                    if line == str(team):
+                        is_registered = True
+                        break
+
+
+        if is_registered:
+            await ctx.send(f'Team {team} has already been registered to this tournament.')
+
+        else:
+            with open("team_list.txt", "a") as file:
+                file.write(str(team) + "\n")
+
+            await ctx.send(f'Team {team} has been successfully registered to this tournament.\nGood Luck!')
+
+    @commands.command()
+    async def clear(self, ctx, team = None):
+        if team is None:
+            with open("team_list.txt", "w") as file:
+                file.truncate()
+
+            await ctx.send("Successfully cleared list of teams")
+        else:
+            teams = []
+            with open("team_list.txt", "a+") as file:
+                teams = [line for line in file.readlines() if line.lower() != str(team).lower()]
+                file.truncate()
+                for t in teams:
+                    file.write(str(t) + "\n")
+
+            await ctx.send(f'Successfully removed team {team} from the team list')
+
+    @commands.command()
+    async def list(self, ctx):
+        file = open("team_list.txt", 'r')
+
+        await ctx.send(f'```{file.read()}```' )
 
 def setup(bot):
     bot.add_cog(Commands(bot))
